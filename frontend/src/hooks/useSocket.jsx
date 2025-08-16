882a67cae4ee3063fba2249f0828f62bd3854676
Alok Sharma <sharma.alok.tech@gmail.com>
2025-08-16 18:39:31 +0530

    Update useSocket.jsx

--- frontend/src/hooks/useSocket.jsx
+++ frontend/src/hooks/useSocket.jsx
@@ -6,8 +6,6 @@ export default function useSocket() {
   const { user } = useSelector((state) => state.auth);
   const [socket, setSocket] = useState(null);
 
-  console.log("user id",user.id);
-
   useEffect(() => {
     if (user) {
       const s = io("https://chat-backend-2b7d.onrender.com", {
