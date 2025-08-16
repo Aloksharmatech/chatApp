7207093359ab489210065b496d67daec26a849b3
Alok Sharma <sharma.alok.tech@gmail.com>
2025-08-16 16:41:34 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -7,6 +7,7 @@ const isAuthenticated = async (req, res, next) => {
         const token = req.cookies.token;
 
         if (!token) {
+            console.log(token);
             return res.status(401).json({
                 message: 'User not authenticated',
                 success: false
